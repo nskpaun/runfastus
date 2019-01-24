9e5835d437c3d9d3120153702f656c0468a26301
Nathan Spaun <nspaun@fb.com>
2019-01-23 17:14:14 -0800

    Remove lore from main page

--- runfastus/src/pages/index.tsx
+++ runfastus/src/pages/index.tsx
@@ -79,7 +79,10 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: {fileAbsolutePath: {regex: "/.*blog.*/"}}
+    ) {
       edges {
         node {
           excerpt
