be5cc98129ff311c134eba912bcbd2c6facbcf45
Nathan Spaun <nspaun@fb.com>
2019-02-05 07:50:46 -0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f9291f5c6680368616e0.js"
+    "url": "webpack-runtime-435efd22f16c5b010884.js"
   },
   {
     "url": "app.bddaef654c66ded1434d.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a04176807d28942b25378b6d2f870ecb"
+    "revision": "06c9bae824dc841f842c868b7532f300"
   },
   {
     "url": "0-3b09f6c65b46529f329a.js"
