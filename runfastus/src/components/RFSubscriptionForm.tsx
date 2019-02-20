387d2c6bf1b24bb926bf863f9c9f04b5232a9a4d
Nathan Spaun <nspaun@fb.com>
2019-02-20 10:22:05 -0800

    Use better confirmation component

--- runfastus/src/components/RFSubscriptionForm.tsx
+++ runfastus/src/components/RFSubscriptionForm.tsx
@@ -1,16 +1,15 @@
 import addToMailchimp from 'gatsby-plugin-mailchimp';
 import * as React from 'react';
 import * as ReactGA from 'react-ga';
-import {ToastsContainer, ToastsStore, ToastsContainerPosition} from 'react-toasts';
 
 ReactGA.initialize('UA-132813912-1');
 
-const initialState = { email: '' };
+const initialState = { email: '', hasSucceeded: null };
 
-type State = Readonly<typeof initialState>;
+type State = Readonly<{ email: string, hasSucceeded?: boolean }>;
 
 
-interface MailChimpResult {msg: string; result: string;};
+interface MailChimpResult { msg: string; result: string; };
 
 const CTA_TEXT = 'Sign up to stay up to date with the latest Runfastus content';
 
@@ -21,6 +20,38 @@ class RFSubscriptionForm extends React.Component<{}, State> {
   readonly state: State = initialState
 
   render() {
+
+    let confirmationComponent;
+    if (this.state.hasSucceeded) {
+      confirmationComponent = <div
+        style={{
+          display: `flex`,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          backgroundColor: '#A2ED90',
+          color: '#FFFFFF',
+          borderRadius: 5,
+          padding: 12,
+        }}>
+        {'You are now subscribed!'}
+      </div>
+    } else if (this.state.hasSucceeded === null) {
+      confirmationComponent = null;
+    } else {
+      confirmationComponent = <div
+        style={{
+          display: `flex`,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          backgroundColor: '#000000',
+          color: '#FFFFFF',
+          borderRadius: 5,
+          padding: 12,
+        }}>
+        {'Oops something went wrong :('}
+      </div>
+    }
+
     return (
       <div
         style={{
@@ -42,69 +73,69 @@ class RFSubscriptionForm extends React.Component<{}, State> {
         </div>
         <div
           style={{
-          display: `flex`,
-          flexDirection: 'row',
-        }}>
-        <textarea
-          style={{
             display: `flex`,
-            backgroundColor: '#FFFFFF',
-            justifyContent: 'center',
-            color: '#000000',
-            borderWidth: 2, borderColor: '#d6d7da',
-            flexGrow: 1,
-            borderRadius: 10,
-            padding: 12,
-            marginTop: 12,
-            marginRight: 12,
-            height: 60,
-          }}
-          placeholder={'Your email address'}
-          value={this.state.email}
-          onChange={(event) => {
-            this.setState({ email: event.target.value });
-          }} >
-          Email
-        </textarea>
-        <div
-          style={{
-            display: `flex`,
-            backgroundColor: '#CD5C5C',
-            justifyContent: 'center',
-            color: '#FFFFFF',
-            borderRadius: 10,
-            padding: 14,
-            marginTop: 12,
-            height: 60,
-          }}
-          onClick={() => {
-            ReactGA.event({
-              category: 'Subscription',
-              action: 'SubscribeButtonPressed',
-            });
-            addToMailchimp(this.state.email)
-              .then((data: MailChimpResult) => {
-                if (data.result === "success") {
-                  ReactGA.event({
-                    category: 'Subscription',
-                    action: 'SubscribeSucceeded',
-                  });
-                  ToastsStore.success("You are now subscribed!");
-                } else {
-                  ToastsStore.success("Oops something went wrong :(");
-                }
-                console.log(data)
-              })
-              .catch(() => {
-                // unnecessary because Mailchimp only ever
-                // returns a 200 status code
-                // see below for how to handle errors
-              });
+            flexDirection: 'row',
           }}>
-          {SUBSCRIBE_TEXT}
+          <textarea
+            style={{
+              display: `flex`,
+              backgroundColor: '#FFFFFF',
+              justifyContent: 'center',
+              color: '#000000',
+              borderWidth: 2, borderColor: '#d6d7da',
+              flexGrow: 1,
+              borderRadius: 10,
+              padding: 12,
+              marginTop: 12,
+              marginRight: 12,
+              height: 60,
+            }}
+            placeholder={'Your email address'}
+            value={this.state.email}
+            onChange={(event) => {
+              this.setState({ email: event.target.value });
+            }} >
+            Email
+        </textarea>
+          <div
+            style={{
+              display: `flex`,
+              backgroundColor: '#CD5C5C',
+              justifyContent: 'center',
+              color: '#FFFFFF',
+              borderRadius: 10,
+              padding: 14,
+              marginTop: 12,
+              height: 60,
+            }}
+            onClick={() => {
+              ReactGA.event({
+                category: 'Subscription',
+                action: 'SubscribeButtonPressed',
+              });
+              addToMailchimp(this.state.email)
+                .then((data: MailChimpResult) => {
+                  if (data.result === "success") {
+                    ReactGA.event({
+                      category: 'Subscription',
+                      action: 'SubscribeSucceeded',
+                    });
+                    this.setState({ hasSucceeded: true });
+                  } else {
+                    this.setState({ hasSucceeded: false });
+                  }
+                  console.log(data)
+                })
+                .catch(() => {
+                  // unnecessary because Mailchimp only ever
+                  // returns a 200 status code
+                  // see below for how to handle errors
+                });
+            }}>
+            {SUBSCRIBE_TEXT}
+          </div>
         </div>
-      </div>
-      <ToastsContainer store={ToastsStore} position={ToastsContainerPosition.BOTTOM_RIGHT}/>
+        {confirmationComponent}
       </div >
     );
   }
